aaa3dbb17035a39710c692dee0a7c734b7330efb
Alisson Peres <aperes.dev@gmail.com>
2020-06-03 12:53:46 -0300

    Base components files created. Added API call in App.js(just for fun)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: this.props.name || "Loading great meal...",
+      image: logo
+    };
+  }
+
+  componentDidMount() {
+    try {
+      fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+        .then(resp => resp.json())
+        .then(data => {
+          const { strMeal, strMealThumb } = data.meals[0];
+          this.setState({ name: strMeal, image: strMealThumb });
+        });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  componentWillMount() {
+    return <div>Will mount</div>;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={this.state.image} className="App-logo" alt="logo" />
+
+          <p>{this.state.name}</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
